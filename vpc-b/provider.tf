b73b5ee616ad6d82db42222f6edea8a1c1d74881
John Macina <jmacina@bradford-corp.com>
2024-07-19 15:00:45 -0500

    rol added and public key path corrected

--- vpc-b/provider.tf
+++ vpc-b/provider.tf
@@ -2,4 +2,9 @@ provider "aws" {
   region     = var.region
   access_key = var.access_key
   secret_key = var.secret_key
+  
+  assume_role {
+    role_arn     = "arn:aws:iam::905418007870:role/tf_vpc_full"
+    session_name ="terraform"
+  }
 }
